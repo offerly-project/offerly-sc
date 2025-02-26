5a7a3a9fcf01a9f4bae20413807102d3abbd91e6
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-02-26 22:08:06 +0400

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ import { env } from "./config";
 import { ScrapeController } from "./controller";
 import { connectToDB } from "./db";
 import { authMiddleware } from "./middlewares";
-import { repository } from "./repository";
-import { SabScrapper } from "./scrappers/KSA/Sab";
-import { createDrivers, launchDrivers } from "./utils";
 (async function () {
 	connectToDB();
 	const app = express();
@@ -33,11 +30,11 @@ import { createDrivers, launchDrivers } from "./utils";
 	});
 })();
 
-(async function () {
-	const drivers = createDrivers();
-	await launchDrivers(drivers);
-	const scrapper = new SabScrapper(drivers);
-	const offers = await repository.getBankOffers("sab");
-	const delta = await scrapper.getDelta(offers);
-	console.log(delta);
-})();
+// (async function () {
+// 	const drivers = createDrivers();
+// 	await launchDrivers(drivers);
+// 	const scrapper = new SabScrapper(drivers);
+// 	const offers = await repository.getBankOffers("sab");
+// 	const delta = await scrapper.getDelta(offers);
+// 	console.log(delta);
+// })();
